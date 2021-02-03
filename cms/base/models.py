53fef3e9594c573251959f980187cb0acbf5352a
todatasudata <vitaly.esipov@gmail.com>
2021-02-03 10:40:24 +0300

    Добавил изображение к домашней странице

--- cms/base/models.py
+++ cms/base/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
 from wagtail.api import APIField
--- cms/home/models.py
+++ cms/home/models.py
@@ -1,32 +1,39 @@
 from django.db import models
 
-
 from wagtail.core.models import Page
-from wagtail.core.fields import StreamField, RichTextField
+from wagtail.core.fields import StreamField
 from wagtail.core import blocks
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.api import APIField
 
 from wagtail.admin.edit_handlers import (
     FieldPanel,
     StreamFieldPanel,
     MultiFieldPanel,
-    InlinePanel,
     RichTextField
 )
+from wagtail.images.api.fields import ImageRenditionField
 
 
 class HomePage(Page):
+    """Домашняя страница"""
+
     max_count = 1
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True
+    )
     text = RichTextField(blank=True, verbose_name='Текст')
     text_sign = RichTextField(max_length=250, blank=True, verbose_name='Подпись')
     ads = StreamField([
         ('ad', blocks.RichTextBlock(help_text='Объявление', label='Объявление'))
-], blank=True)
+    ], blank=True)
 
     content_panels = Page.content_panels + [
+        ImageChooserPanel('image', heading='Иллюстрация'),
         MultiFieldPanel([
-        FieldPanel('text', heading='Текст'),
-        FieldPanel('text_sign', heading='Подпись')], heading='Описание'),
+            FieldPanel('text', heading='Текст'),
+            FieldPanel('text_sign', heading='Подпись')],
+            heading='Описание'),
         StreamFieldPanel('ads', heading='Объявления')
     ]
 
@@ -34,8 +41,9 @@ class HomePage(Page):
         APIField("text"),
         APIField("text_sign"),
         APIField("ads"),
+        APIField("image", serializer=ImageRenditionField('fill-200x250')),
+        APIField("image_small", serializer=ImageRenditionField('fill-50x50', source='image'))
     ]
 
     class Meta:
         verbose_name = 'Главная страница'
-
